fd9836aba55f4eb68cf66da500ae33cd3ed8fe75
rs <1984476386@qq.com>
2021-09-21 20:51:29 +0800

    hot-fix first commit

--- git/src/main/java/com/zrs/git/GitTest.java
+++ git/src/main/java/com/zrs/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
         System.out.println("hello，git1");
         System.out.println("hello，git2");
         System.out.println("hello，git3");
-        System.out.println("hello，git4(hot-fix)");
+        System.out.println("hello，git4-1(hot-fix)");
+        System.out.println("hello，git4-2(hot-fix)");
     }
 }
